24b2aa28872d5c6965ea3709304b0546b08cfdb2
Juan Oliveira <ruantos@proton.me>
2025-10-27 19:09:01 -0300

    refactor: refactoring extract file

--- src/desafio_tech/extract.py
+++ src/desafio_tech/extract.py
@@ -4,26 +4,26 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-ENDPOINT = "http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?sql="
+DATASTORE_ENDPOINT = "http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?sql="
+DATASET_ENDPOINT = "http://dados.recife.pe.gov.br/api/3/action/package_show?id=acidentes-de-transito-com-e-sem-vitimas"
 QUERY = "SELECT * FROM "
-DATASET = "http://dados.recife.pe.gov.br/api/3/action/package_show?id=acidentes-de-transito-com-e-sem-vitimas"
-identifier = "d153f88e-3c25-422b-8b94-ef8d660bf7bf"
 
 
 def get_resources_ids() -> list[str]:
 	try:
-		response = requests.get(url=DATASET, timeout=60)
+		response = requests.get(url=DATASET_ENDPOINT, timeout=60)
 		resources = response.json()['result']['resources']
-		return [resources[i]['id'] for i in range( len(resources) )]
+		return [resources[i]['id'] for i in range( len(resources) ) if resources[i]['datastore_active']]
 
 	except requests.exceptions.RequestException as e:
 		logger.warning(f"An error occurred while trying to fetch ids: {e}")
 		return []
 
 
-def fetch_dataframes(identifier: str) -> pd.DataFrame:
+def fetch_dataframe(identifier: str) -> pd.DataFrame:
 	query = f'SELECT * FROM "{identifier}"'
-	url = f'{ENDPOINT}{query}'
+	url = f'{DATASTORE_ENDPOINT}{query}'
+
 	try:
 		response = requests.get(url=url, timeout=60)
 		records = response.json()["result"]["records"]
@@ -33,8 +33,6 @@ def fetch_dataframes(identifier: str) -> pd.DataFrame:
 
 		return pd.DataFrame(records)
 
-
-
 	except KeyError as e:
 		logger.warning(f'No records found for dataset: {identifier}\nError: {e}')
 
@@ -42,8 +40,10 @@ def fetch_dataframes(identifier: str) -> pd.DataFrame:
 		logger.warning(f"An error occurred while trying to fetch dataframe: {e}")
 
 
-
 if __name__ == "__main__":
 	ids = get_resources_ids()
+	dataframes = []
 	for id in ids:
-		print(type(fetch_dataframes(id)))
+		dataframes.append(fetch_dataframe(id))
+
+	print(dataframes)
