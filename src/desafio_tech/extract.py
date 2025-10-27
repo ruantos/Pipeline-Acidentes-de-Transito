531eb0a2e3ea67800753ee263e1d3f9358ec15ec
Juan Oliveira <ruantos@proton.me>
2025-10-27 20:17:46 -0300

    feat: creates save dataframe function

--- src/desafio_tech/extract.py
+++ src/desafio_tech/extract.py
@@ -1,6 +1,9 @@
+from os.path import exists
+
 import requests
 import pandas as pd
 import logging
+import os
 
 
 logging.basicConfig(level=logging.INFO)
@@ -42,7 +45,31 @@ def fetch_dataframe(identifier: str) -> pd.DataFrame:
 		return pd.DataFrame()
 
 
+def save_dataframe(df: pd.DataFrame):
+	path = "../../data/bronze/"
+	os.makedirs(path, exist_ok=True)
+
+	try:
+
+		df.columns = [column.lower() for column in df.columns]
+		if 'data' not in df.columns:
+			raise ValueError("Dataframe must not have the column DATA or have a empty column ")
+
+		year = str(df['data'].iloc[0].split("-")[0])
+		file_path = os.path.join(path, f"acidentes_de_transito_{year}.parquet")
+
+		df.to_parquet(path=file_path, engine="pyarrow")
+		logger.info(f"Dataframe saved successufuly at {file_path}")
+
+	except Exception as e:
+		logger.warning(f"An error occurred while trying to save the dataframe: {e}")
+
+
+
 if __name__ == "__main__":
 	ids = get_resources_ids()
 	dataframes = [fetch_dataframe(identifier) for identifier in ids]
+	for df in dataframes:
+		save_dataframe(df)
+
 
