03ec5ac3c1b0050a603a0a0a8a4afb9bf36397bc
Juan Oliveira <ruantos@proton.me>
2025-10-27 16:56:59 -0300

    feat: creates function get_ids

--- src/desafio_tech/extract.py
+++ src/desafio_tech/extract.py
@@ -5,23 +5,40 @@ import logging
 
 logger = logging.getLogger(__name__)
 ENDPOINT = "http://dados.recife.pe.gov.br/api/3/action/datastore_search_sql?"
-ENDPOINT_LIST = "http://dados.recife.pe.gov.br/api/3/action/package_list"
+ENDPOINT_NAME = "http://dados.recife.pe.gov.br/api/3/action/package_list"
+ENDPOINT_ID = "http://dados.recife.pe.gov.br/api/3/action/package_show?id"
 identifier = "d153f88e-3c25-422b-8b94-ef8d660bf7bf"
+
+
+def get_sets_ids(name_list: list[str]) -> list[str]:
+	id_list = []
+
+	for name in name_list:
+		try:
+			response = requests.get(f"{ENDPOINT_ID}{name}", timeout=60)
+			id_list.append(response.json())
+
+		except requests.exceptions.RequestException as e:
+			logger.warning(f"An error occurred while trying to fetch ids: {e}")
+			return []
+
+	return id_list
 
 
 def get_sets_names() -> list[str] | None:
 	word = 'acidentes'
 	try:
-		response = requests.get(url=ENDPOINT_LIST, timeout=60)
+		response = requests.get(url=ENDPOINT_NAME, timeout=60)
 		datastore = response.json()["result"]
 
 		if not datastore:
 			logger.warning(f'No datasets found in the datastore for phrase: {word}')
 			return []
-		return [dataset for dataset in datastore if dataset.startswith(word)]
+		return [ds_name for ds_name in datastore if ds_name.startswith(word)]
 
 	except requests.exceptions.RequestException as e:
 		logger.warning(f"An error occurred while trying to fetch dataframe: {e}")
+
 
 
 
