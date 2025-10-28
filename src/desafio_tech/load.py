4e77efe032e57cadf20440cf7472d1ddb66eee4d
Juan Oliveira <ruantos@proton.me>
2025-10-28 12:48:48 -0300

    feat: creates filters_columns function

--- src/desafio_tech/load.py
+++ src/desafio_tech/load.py
@@ -1,5 +1,15 @@
 import psycopg
 import pandas as pd
 
+
+def filters_columns(df: pd.DataFrame) -> pd.DataFrame:
+	used_cols = ['natureza', 'tipo', 'vitimas', 'hora', 'data', 'bairro',
+	             'ciclista', 'moto','caminhao', 'auto', 'pedestre','onibus',
+                 'endereco', 'viatura']
+
+	return df[used_cols]
+
+
 def load(df: pd.Dataframe):
-	pass
+	df = filters_columns(df)
+
