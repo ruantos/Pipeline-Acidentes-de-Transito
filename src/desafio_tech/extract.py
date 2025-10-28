484c444a49b136e429f9501c7d9e25eec25b49c5
Juan Oliveira <ruantos@proton.me>
2025-10-28 20:31:26 -0300

    feat: adds normalize_names function

--- src/desafio_tech/extract.py
+++ src/desafio_tech/extract.py
@@ -36,7 +36,9 @@ def fetch_dataframe(identifier: str) -> pd.DataFrame:
 			logger.info(f'No records found for dataset: {identifier}')
 
 		logger.info(f"Dataframe fetched successfully\n")
-		return pd.DataFrame(records)
+
+		df = pd.DataFrame(records)
+		return normalize_names(df)
 
 	except KeyError as e:
 		logger.warning(f'No records found for dataset: {identifier}\nError: {e}')
@@ -47,30 +49,6 @@ def fetch_dataframe(identifier: str) -> pd.DataFrame:
 		return pd.DataFrame()
 
 
-def save_dataframe(df: pd.DataFrame):
-	path = "../../data/bronze/"
-	os.makedirs(path, exist_ok=True)
-
-	try:
-
-		df.columns = [column.lower() for column in df.columns]
-		if 'data' not in df.columns:
-			raise ValueError("Dataframe must not have the column DATA or have a empty column ")
-
-		year = str(df['data'].iloc[0].split("-")[0])
-		file_path = os.path.join(path, f"acidentes_de_transito_{year}.parquet")
-
-		df.to_parquet(path=file_path, engine="pyarrow")
-		logger.info(f"Dataframe saved successufuly at {file_path}")
-
-	except Exception as e:
-		logger.warning(f"An error occurred while trying to save the dataframe: {e}")
-
-
-if __name__ == "__main__":
-	ids = get_ids()
-	dataframes = [fetch_dataframe(identifier) for identifier in ids]
-	for df in dataframes:
-		save_dataframe(df)
-
-
+def normalize_names(df: pd.DataFrame) -> pd.DataFrame:
+	df.columns = [col.lower() for col in df.columns]
+	return df
